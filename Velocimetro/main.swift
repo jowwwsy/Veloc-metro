90420eed5ae7fd1fe4081a4b333d56712e8fc65f
Joselyne P Banegas <joselynepalmabanegas@macbook-pro-de-joselyne.local>
2016-09-08 00:38:17 -0300

    Initial Commit

---
+++ Velocimetro/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Velocimetro
+//
+//  Created by Joselyne Banegas on 08/09/16.
+//  Copyright © 2016 Joselyne Banegas. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
