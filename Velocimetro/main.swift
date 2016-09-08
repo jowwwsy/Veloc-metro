7985b52672678457a24605e0abf85f1245608780
Joselyne P Banegas <joselynepalmabanegas@macbook-pro-de-joselyne.local>
2016-09-08 00:48:35 -0300

    alterando

--- Velocimetro/main.swift
+++ Velocimetro/main.swift
@@ -8,5 +8,46 @@
 
 import Foundation
 
-print("Hello, World!")
 
+enum Velocidades:Int{
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    
+    init(velocidadInicial:Velocidades){
+        self = velocidadInicial
+    }
+    
+}
+
+class Auto {
+    var velocidad:Velocidades
+    init(){
+        velocidad = Velocidades(velocidadInicial: .Apagado)
+    }
+    
+    func cambioDeVelocidad() -> ( actual : Int, velocidadEnCadena: String){
+        var legenda = ""
+        let actual = velocidad.rawValue
+        switch velocidad {
+        case .Apagado:
+            velocidad = .VelocidadBaja
+            legenda = "Apagado"
+        case .VelocidadBaja:
+            velocidad = .VelocidadMedia
+            legenda = "Velocidad Baja"
+        case .VelocidadMedia:
+            velocidad = .VelocidadAlta
+            legenda = "Velocidad Media"
+        case .VelocidadAlta:
+            velocidad = .Apagado
+            legenda = "Velocidad Alta"
+        }
+        return (actual, legenda)
+    }
+    
+}
+
+let auto = Auto()
+for i in 1...20 {
+    let result = auto.cambioDeVelocidad()
+    print("\(i). \(result.actual), \(result.velocidadEnCadena)")
+}
